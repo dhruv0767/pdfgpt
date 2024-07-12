b19ab20ed7fe95e974de5ba81e2d8fa79db067b1
Dhruv Shah <dhruv076787@gmail.com>
2024-07-12 19:24:42 +0530

    v1

---
+++ constant.py
@@ -0,0 +1,10 @@
+import os 
+from chromadb.config import Settings
+
+
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+        chroma_db_impl='duckdb+parquet',
+        persist_directory='db',
+        anonymized_telemetry=False
+)
